54a4b5f32f48e76fed8befd7385384cb14d07eb4
Mauro Doglio <mdoglio@mozilla.com>
2017-05-16 16:47:00 +0100

    Standardize case classes indentation style

--- src/main/scala/com/mozilla/telemetry/pings/package.scala
+++ src/main/scala/com/mozilla/telemetry/pings/package.scala
@@ -11,37 +11,33 @@ import org.json4s.jackson.JsonMethods.parse
 
 package object pings {
   case class Application(
-                          architecture: String,
-                          buildId: String,
-                          channel: String,
-                          name: String,
-                          platformVersion: String,
-                          vendor: String,
-                          version: String,
-                          xpcomAbi: String
-                        )
+      architecture: String,
+      buildId: String,
+      channel: String,
+      name: String,
+      platformVersion: String,
+      vendor: String,
+      version: String,
+      xpcomAbi: String)
 
   case class Build(
-                    applicationId: Option[String],
-                    applicationName: Option[String],
-                    architecture: String,
-                    buildId: String,
-                    platformVersion: String,
-                    vendor: String,
-                    version: String,
-                    xpcomAbi: String
-                  )
+      applicationId: Option[String],
+      applicationName: Option[String],
+      architecture: String,
+      buildId: String,
+      platformVersion: String,
+      vendor: String,
+      version: String,
+      xpcomAbi: String)
 
   case class SystemOs(name: String, version: String)
 
   case class SystemGfxFeatures(compositor: Option[String])
 
   case class SystemGfx(
-                        D2DEnabled: Option[Boolean],
-                        DWriteEnabled: Option[Boolean],
-                        features: Option[SystemGfxFeatures]
-                      )
-
+      D2DEnabled: Option[Boolean],
+      DWriteEnabled: Option[Boolean],
+      features: Option[SystemGfxFeatures])
 
   case class System(os: SystemOs, gfx: Option[SystemGfx])
 
@@ -50,55 +46,50 @@ package object pings {
   case class Addons(activeExperiment: Option[ActiveExperiment])
 
   case class Settings(
-                       blocklistEnabled: Option[Boolean],
-                       isDefaultBrowser: Option[Boolean],
-                       e10sEnabled: Option[Boolean],
-                       e10sCohort: Option[String],
-                       locale: Option[String],
-                       telemetryEnabled: Option[Boolean]
-                     )
+      blocklistEnabled: Option[Boolean],
+      isDefaultBrowser: Option[Boolean],
+      e10sEnabled: Option[Boolean],
+      e10sCohort: Option[String],
+      locale: Option[String],
+      telemetryEnabled: Option[Boolean])
 
-  case class Profile(
-                      creationDate: Option[Int],
-                      resetDate: Option[Int]
-                    )
+  case class Profile(creationDate: Option[Int], resetDate: Option[Int])
 
   case class Meta(
-                   Host: Option[String],
-                   Hostname: Option[String],
-                   Size: Option[Double],
-                   Timestamp: Long,
-                   Type: Option[String],
-                   appBuildId: String,
-                   appName: String,
-                   appUpdateChannel: Option[String],
-                   appVendor: Option[String],
-                   appVersion: String,
-                   clientId: Option[String],
-                   creationTimestamp: Option[Float],
-                   docType: Option[String],
-                   documentId: Option[String],
-                   geoCity: Option[String],
-                   geoCountry: String,
-                   normalizedChannel: String,
-                   os: String,
-                   sampleId: Option[Double],
-                   sourceName: Option[String],
-                   sourceVersion: Option[Int],
-                   submissionDate: String,
-                   telemetryEnabled: Option[Boolean],
-                   // Common fields preparsed by hindsight
-                   `environment.build`: Option[EnvironmentBuild],
-                   `environment.settings`: Option[Settings],
-                   `environment.system`: Option[System],
-                   `environment.profile`: Option[Profile],
-                   `environment.addons`: Option[Addons],
-                   // Main ping fields preparsed by hindsight
-                   `payload.simpleMeasurements`: JValue,
-                   `payload.keyedHistograms`: JValue,
-                   `payload.histograms`: JValue,
-                   `payload.info`: JValue
-                 ){
+      Host: Option[String],
+      Hostname: Option[String],
+      Size: Option[Double],
+      Timestamp: Long,
+      Type: Option[String],
+      appBuildId: String,
+      appName: String,
+      appUpdateChannel: Option[String],
+      appVendor: Option[String],
+      appVersion: String,
+      clientId: Option[String],
+      creationTimestamp: Option[Float],
+      docType: Option[String],
+      documentId: Option[String],
+      geoCity: Option[String],
+      geoCountry: String,
+      normalizedChannel: String,
+      os: String,
+      sampleId: Option[Double],
+      sourceName: Option[String],
+      sourceVersion: Option[Int],
+      submissionDate: String,
+      telemetryEnabled: Option[Boolean],
+      // Common fields preparsed by hindsight
+      `environment.build`: Option[EnvironmentBuild],
+      `environment.settings`: Option[Settings],
+      `environment.system`: Option[System],
+      `environment.profile`: Option[Profile],
+      `environment.addons`: Option[Addons],
+      // Main ping fields preparsed by hindsight
+      `payload.simpleMeasurements`: JValue,
+      `payload.keyedHistograms`: JValue,
+      `payload.histograms`: JValue,
+      `payload.info`: JValue) {
 
     /**
       * Returns a java Timestamp obj with microseconds resolution.
@@ -110,20 +101,18 @@ package object pings {
   }
 
   case class CrashPayload(
-                           crashDate: String,
-                           processType: Option[String],
-                           hasCrashEnvironment: Option[Boolean],
-                           metadata: Option[Map[String, String]],
-                           version: Option[Int]
-                         )
+      crashDate: String,
+      processType: Option[String],
+      hasCrashEnvironment: Option[Boolean],
+      metadata: Option[Map[String, String]],
+      version: Option[Int])
 
   case class CrashPing(
-                        application: Application,
-                        clientId: Option[String],
-                        payload: CrashPayload,
-                        // Environment is omitted it's partially available under meta
-                        meta: Meta
-                      ){
+      application: Application,
+      clientId: Option[String],
+      payload: CrashPayload,
+      // Environment is omitted it's partially available under meta
+      meta: Meta) {
 
     def isMainCrash: Boolean = {
       payload.processType.getOrElse("main") == "main"
@@ -133,11 +122,10 @@ package object pings {
   case class PayloadInfo(subsessionLength: Option[Int])
 
   case class MainPing(
-                       application: Application,
-                       clientId: Option[String],
-                       // Environment omitted because it's mostly available under meta
-                       meta: Meta
-                     ){
+      application: Application,
+      clientId: Option[String],
+      // Environment omitted because it's mostly available under meta
+      meta: Meta) {
     def getCountHistogramValue(histogram_name: String): Option[Int] = {
       try {
         this.meta.`payload.histograms` \ histogram_name \ "values" \ "0" match {
@@ -172,16 +160,13 @@ package object pings {
   case class Environment(build: EnvironmentBuild, system: EnvironmentSystem)
 
   case class EnvironmentBuild(
-                               version: Option[String],
-                               buildId: Option[String],
-                               architecture: Option[String]
-                             )
+      version: Option[String],
+      buildId: Option[String],
+      architecture: Option[String])
 
   case class EnvironmentSystem(os: OS)
 
-  case class OS(
-                 name: Option[String],
-                 version: Option[String])
+  case class OS(name: Option[String], version: Option[String])
 
   def messageToCrashPing(message: Message): CrashPing = {
     implicit val formats = DefaultFormats
@@ -227,5 +212,4 @@ package object pings {
       case _ => JObject()
     }
   }
-
 }
