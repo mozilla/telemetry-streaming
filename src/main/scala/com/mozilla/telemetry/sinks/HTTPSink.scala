f7f6594fc74af267148ee942c7a31135b7889750
Frank Bertsch <fbertsch@mozilla.com>
2018-03-26 23:15:45 -0500

    Make HTTP sink more resilient to errors
    This handles:
    - Retrying after timeouts
    - Retrying after server errors
    - Logging on failures

--- src/main/scala/com/mozilla/telemetry/sinks/HTTPSink.scala
+++ src/main/scala/com/mozilla/telemetry/sinks/HTTPSink.scala
@@ -5,8 +5,24 @@ package com.mozilla.telemetry.streaming.sinks
 
 import scalaj.http.{Http, HttpRequest}
 import org.apache.spark.sql.ForeachWriter
+import scala.annotation.tailrec
+import scala.util.{Try, Success, Failure}
 
-class HttpSink[String](url: String, parameters: Map[String, String]) extends ForeachWriter[String] {
+class HttpSink[String](url: String, parameters: Map[String, String], maxAttempts: Int = 5, defaultDelay: Int = 500, connectionTimeout: Int = 2000)
+  extends ForeachWriter[String] {
+
+  // codes to retry a request on - allow retries on timeouts
+  // docs: https://amplitude.zendesk.com/hc/en-us/articles/204771828#http-status-codes-retrying-failed-requests
+
+  val TimeoutPseudoCode = -1
+  val RetryCodes = TimeoutPseudoCode :: 429 :: 500 :: 502 :: 503 :: 504 :: Nil
+  val OK = 200
+
+  // timeouts in ms
+  val ReadTimeout = 5000
+
+  @transient lazy val log = org.apache.log4j.LogManager.getLogger("HttpSinkLogger")
+
   def close(errorOrNull: Throwable): Unit = {}
   def open(partitionId: Long,version: Long): Boolean = true
 
@@ -17,8 +33,32 @@ class HttpSink[String](url: String, parameters: Map[String, String]) extends For
   }
 
   def process(event: String): Unit = {
-    getRequest
-      .param("event", event.toString)
-      .asString
+    attempt(
+      getRequest
+        .param("event", event.toString)
+        .timeout(connTimeoutMs = connectionTimeout, readTimeoutMs = ReadTimeout))
+  }
+
+  private def backoff(tries: Int): Long = (scala.math.pow(2, tries) - 1).toLong * defaultDelay
+
+  @tailrec
+  private def attempt(request: HttpRequest, tries: Int = 0): Unit = {
+    if(tries > 0) { // minor optimization
+      java.lang.Thread.sleep(backoff(tries))
+    }
+
+    val code = Try(request.asString.code) match {
+      case Success(c) => c
+      case Failure(e: java.net.SocketTimeoutException) => TimeoutPseudoCode
+    }
+
+    (code, tries == maxAttempts) match {
+      case (OK, _) =>
+      case (c, false) if RetryCodes.contains(c)=> attempt(request, tries + 1)
+      case (c, _) => {
+        val url = request.url + "?" + request.params.map{ case(k, v) => s"k=v" }.mkString("&")
+        log.warn(s"Failed request $url, last status code: $c")
+      }
+    }
   }
 }
