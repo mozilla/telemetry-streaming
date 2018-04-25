7fc65a5970d66b33dbc489e007a3a9ed8c282c7e
Frank Bertsch <fbertsch@mozilla.com>
2018-04-25 09:59:44 -0500

    WIP: strip colon

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | bash
 
 export ARTIFACTS_PERMISSIONS="public-read"
-export ARTIFACTS_PATHS="$(find $TOP_LEVEL_JAR_DIR -type f | tr "\n" ":")"
+export ARTIFACTS_PATHS="$(find $TOP_LEVEL_JAR_DIR -type f | tr "\n" ":" | sed -e 's/:$//')"
 artifacts upload
