206f4c9c94f389df21e560c89bfb4e9359f61ee1
Frank Bertsch <fbertsch@mozilla.com>
2018-04-25 12:58:54 -0500

    Work around colon in target paths

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | 
 
 export ARTIFACTS_PERMISSIONS="public-read"
 export ARTIFACTS_PATHS="$TOP_LEVEL_JAR_DIR/"
+export ARTIFACTS_TARGET_PATHS="/$TOP_LEVEL_JAR_DIR/"
 artifacts upload
