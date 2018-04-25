99c30ebe155e3ecbbc356fff2d632e807255cbbf
Frank Bertsch <fbertsch@mozilla.com>
2018-04-25 09:07:33 -0500

    Ugh.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 curl -sL https://raw.githubusercontent.com/travis-ci/artifacts/master/install | bash
-tar -C dist -zcvf all.tgz .
 
 export ARTIFACTS_PERMISSIONS="public-read"
 export ARTIFACTS_PATHS="$(find $TOP_LEVEL_JAR_DIR -type f | tr "\n" ":")"
