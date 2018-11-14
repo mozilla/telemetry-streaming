20040e3170b9e6a53b4335ca014d63b7c4691512
Sunah Suh <ssuh@mozilla.com>
2018-11-14 13:39:28 -0600

    Bug 1507238: Cast sample_id to int before converting to string

--- src/test/scala/com/mozilla/telemetry/streaming/EventPingEventsTest.scala
+++ src/test/scala/com/mozilla/telemetry/streaming/EventPingEventsTest.scala
@@ -29,4 +29,25 @@ class EventPingEventsTest extends FlatSpec with Matchers with DataFrameSuiteBase
           "session_id" -> "{78fe2428-15fb-4448-b517-cbb85f22def0}", "page" -> "about:newtab")), "parent"))
   }
 
+  "Event Ping Events" should "output sampleIds without decimal points" in {
+    import spark.implicits._
+
+    val messageDF = spark.sparkContext.parallelize(
+      TestUtils.generateEventMessages(1, fieldsOverride = Some(Map("sampleId" -> 42L))).map(_.toByteArray)
+    ).toDF("value")
+
+    val results = EventPingEvents.explodeEvents(messageDF).persist
+
+    results.count should be(4)
+    results.filter("event_process = 'parent'").count == 2
+    results.filter("event_process = 'dynamic'").count == 2
+    results.first should be(
+      EventPingEvents.EventRow("an_id", "client1", "release", "IT", None, "Firefox", "42.0", Some("Linux"), Some("42"),
+        "dd302e9d-569b-4058-b7e8-02b2ff83522c", "79a2728f-af12-4ed3-b56d-0531a03c2f26", 1530291900000L, 1460036116,
+        Some("42"), Some(Map("experiment2" -> "chaos", "experiment1" -> "control")), 4118829, "activity_stream", "end",
+        "session", Some("909"),
+        Some(Map("addon_version" -> "2018.06.22.1337-8d599e17", "user_prefs" -> "63",
+          "session_id" -> "{78fe2428-15fb-4448-b517-cbb85f22def0}", "page" -> "about:newtab")), "parent"))
+  }
+
 }
