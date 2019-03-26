7befb029beebe568a33747db68d5b51a5e2f0e47
Victor Ng <victor@crankycoder.com>
2019-03-26 18:04:14 -0400

    Added filter to drop frecency pings that include the substring 'not-submitting'

--- src/main/scala/com/mozilla/telemetry/streaming/FederatedLearningSearchOptimizer.scala
+++ src/main/scala/com/mozilla/telemetry/streaming/FederatedLearningSearchOptimizer.scala
@@ -74,7 +74,13 @@ object FederatedLearningSearchOptimizer extends StreamingJobBase {
         val docType = fields.getOrElse("docType", "").asInstanceOf[String]
         if ("frecency-update" == docType) {
           val ping = FrecencyUpdatePing(m)
-          if ((ping.payload.study_variation startsWith variationTarget) && (ping.payload.bookmark_and_history_num_suggestions_displayed > -1)) {
+          if (
+              (
+               (ping.payload.study_variation startsWith variationTarget) &&
+              !(ping.payload.study_variation contains "not-submitting")
+              ) &&
+              ( ping.payload.bookmark_and_history_num_suggestions_displayed > -1)
+            ) {
             Option(FrecencyUpdate(
               new Timestamp(clock.millis()),
               ping.payload.model_version,
